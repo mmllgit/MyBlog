f21c82f834bfee00cb5fda3326bee82e6f0f6e7a
邓先阆 <1151390492@qq.com>
2022-08-28 10:38:30 +0800

    修改

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseURL = "http://localhost:3000/api";
+// export const baseURL = "http://localhost:3000/api";
+export const baseURL = "https://my-blog-iota-ashy.vercel.app/api/upload";
