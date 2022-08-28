5c877cdf58b744df589a6660413b3db0ced12120
邓先阆 <1151390492@qq.com>
2022-08-28 10:22:13 +0800

    修改

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,5 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import multer from "multer";
+import NextCors from "nextjs-cors";
+import mysql from "mysql2";
+import path from "path";
+require("dotenv").config();
+
+const postsDirectory = path.join(process.cwd(), "posts");
 
 function runMiddleware(
   req: NextApiRequest & { [key: string]: any },
@@ -37,10 +43,15 @@ export default async function handler(
   req: NextApiRequest & { [key: string]: any },
   res: NextApiResponse
 ) {
+  await NextCors(req, res, {
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  });
   if (req.method === "POST") {
     const storage = multer.diskStorage({
       destination: function (req, file, cb) {
-        cb(null, "posts");
+        cb(null, postsDirectory);
       },
       filename: function (req, file, cb) {
         cb(null, file.originalname);
@@ -62,11 +73,16 @@ export default async function handler(
       },
     });
     await runMiddleware(req, res, multerUpload.single("file"));
+    const connection = mysql.createConnection(
+      `mysql://hztzo6dt2tpp:pscale_pw_maV4a1hl0AyXxRoQZk_61HTYVLX2swVaE4j9ogzjmj4@ogzvytsuaai4.us-east-3.psdb.cloud/dxlblog?ssl={"rejectUnauthorized":true}`
+    );
+    console.log("Connected to PlanetScale!");
     res.status(200).json({
       code: 200,
       data: null,
       msg: "上传成功",
     });
+    connection.end();
   } else {
     res.status(400).json({
       code: 400,
